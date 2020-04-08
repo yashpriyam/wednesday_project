bf12cca7a58534a90907fe405fccd71fe034c710
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-04-08 17:08:36 +0530

    Updates

---
+++ precache-manifest.7f29fa4bcd200bf18f6a99df016214df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef3103651d8ecc9065433c84c49b09b",
+    "url": "/wednesday_project/index.html"
+  },
+  {
+    "revision": "608fbe7732665439ce14",
+    "url": "/wednesday_project/static/css/main.47a787d5.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/wednesday_project/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "608fbe7732665439ce14",
+    "url": "/wednesday_project/static/js/main.f415bf21.chunk.js"
+  },
+  {
+    "revision": "d8b4c7bcb1d626fd7e62",
+    "url": "/wednesday_project/static/js/runtime~main.22ac4f87.js"
+  }
+]);
